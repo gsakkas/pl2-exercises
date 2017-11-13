dc9f586f33347af8862259e007b134a7cca6aae5
gsakkas_git <george.p.sakkas@gmail.com>
2017-11-13 15:21:38 +0200

    Finished Exercise 2

--- Exercise_2/foldTree.hs
+++ Exercise_2/foldTree.hs
@@ -1,6 +1,6 @@
 module Main where
 
-data Tree a = T a [Tree a]
+data Tree a = T a [Tree a] deriving (Show)
 
 foldTree :: (a -> [b] -> b) -> Tree a -> b
 foldTree f (T x ts) = f x (map (foldTree f) ts)
@@ -33,6 +33,31 @@ nodes :: Tree a -> [a]
 nodes = foldTree f
     where f x bs = foldl (++) [x] bs
 
+countTree :: (a -> Bool) -> Tree a -> Integer
+countTree f = foldTree f'
+    where f' x bs
+            | f x == True = 1 + foldr (+) 0 bs
+            | otherwise = foldr (+) 0 bs
+
+leaves :: Tree a -> [a]
+leaves = foldTree f
+    where f x [] = [x]
+          f _ bs = foldl (++) [] bs
+
+mapTree :: (a -> b) -> Tree a -> Tree b
+mapTree f = foldTree f'
+    where f' x bs = T (f x) bs
+
+trimTree :: Int -> Tree a -> Tree a
+trimTree 1 (T x _) = (T x [])
+trimTree n (T x ts) = T x (map (trimTree (n-1)) ts)
+
+path :: [Int] -> Tree a -> a
+path [] (T y _) = y
+path (x:xs) (T _ ts) = path xs (findXth x ts)
+    where findXth 0 (x:_) = x
+          findXth x (_:xs) = findXth (x-1) xs
+
 main =
     let t = T 10 [T 8 [T 3 [], T 2 []], T 6 [], T 4 [T 1 [T 0 []]]]
 --      10
@@ -42,4 +67,4 @@ main =
 -- 3   2   1
 --         |
 --         0
-    in print $ nodes t
+    in print $ path [2,0,0] t
