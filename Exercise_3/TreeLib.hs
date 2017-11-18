80f06296322d442ad1451ef17e1d90a1aa3f4259
gsakkas_git <george.p.sakkas@gmail.com>
2017-11-18 02:08:47 +0200

    Updated Exercise 3

---
+++ Exercise_3/TreeLib.hs
@@ -0,0 +1,61 @@
+module TreeLib where
+
+data Tree a = T a [Tree a] deriving (Show)
+
+foldTree :: (a -> [b] -> b) -> Tree a -> b
+foldTree f (T x ts) = f x (map (foldTree f) ts)
+
+sizeTree :: Num b => Tree a -> b
+sizeTree = foldTree f
+    where f x bs = 1 + foldr (+) 0 bs
+
+heightTree :: (Ord b, Num b) => Tree a -> b
+heightTree = foldTree f
+    where f x [] = 1
+          f x bs = 1 + maximum bs
+
+sumTree :: Num a => Tree a -> a
+sumTree = foldTree f
+    where f x bs = x + foldr (+) 0 bs
+
+maxTree :: Ord a => Tree a -> a
+maxTree = foldTree f
+    where f x [] = x
+          f x bs = max x (maximum bs)
+
+inTree :: Eq a => a -> Tree a -> Bool
+inTree x = foldTree f
+    where f y bs
+            | x == y = True
+            | otherwise = foldr (||) False bs
+
+nodes :: Tree a -> [a]
+nodes = foldTree f
+    where f x bs = foldl (++) [x] bs
+
+countTree :: (a -> Bool) -> Tree a -> Integer
+countTree f = foldTree f'
+    where f' x bs
+            | f x == True = 1 + foldr (+) 0 bs
+            | otherwise = foldr (+) 0 bs
+
+leaves :: Tree a -> [a]
+leaves = foldTree f
+    where f x [] = [x]
+          f _ bs = foldl (++) [] bs
+
+mapTree :: (a -> b) -> Tree a -> Tree b
+mapTree f = foldTree f'
+    where f' x bs = T (f x) bs
+
+trimTree :: Int -> Tree a -> Tree a
+trimTree 1 (T x _) = (T x [])
+trimTree n (T x ts) = T x (map (trimTree (n-1)) ts)
+
+path :: [Int] -> Tree a -> a
+path [] (T y _) = y
+path (x:xs) (T _ ts) = path xs (findXth x ts)
+    where findXth x _ | x < 0 = error "Negative child in path!"
+          findXth _ xs | null xs = error "Empty children list!"
+          findXth 0 (x:_) = x
+          findXth x (_:xs) = findXth (x-1) xs
--- Exercise_3/quickcheck.hs
+++ Exercise_3/quickcheck.hs
@@ -1,72 +1,9 @@
 module Main where
 
+import TreeLib -- Exercise 2
 import Test.QuickCheck
 import Control.Monad
 import Data.Ratio
-
--- Exercise 2
-
-data Tree a = T a [Tree a] deriving (Show)
-
-foldTree :: (a -> [b] -> b) -> Tree a -> b
-foldTree f (T x ts) = f x (map (foldTree f) ts)
-
-sizeTree :: Num b => Tree a -> b
-sizeTree = foldTree f
-    where f x bs = 1 + foldr (+) 0 bs
-
-heightTree :: (Ord b, Num b) => Tree a -> b
-heightTree = foldTree f
-    where f x [] = 1
-          f x bs = 1 + maximum bs
-
-sumTree :: Num a => Tree a -> a
-sumTree = foldTree f
-    where f x bs = x + foldr (+) 0 bs
-
-maxTree :: Ord a => Tree a -> a
-maxTree = foldTree f
-    where f x [] = x
-          f x bs = max x (maximum bs)
-
-inTree :: Eq a => a -> Tree a -> Bool
-inTree x = foldTree f
-    where f y bs
-            | x == y = True
-            | otherwise = foldr (||) False bs
-
-nodes :: Tree a -> [a]
-nodes = foldTree f
-    where f x bs = foldl (++) [x] bs
-
-countTree :: (a -> Bool) -> Tree a -> Integer
-countTree f = foldTree f'
-    where f' x bs
-            | f x == True = 1 + foldr (+) 0 bs
-            | otherwise = foldr (+) 0 bs
-
-leaves :: Tree a -> [a]
-leaves = foldTree f
-    where f x [] = [x]
-          f _ bs = foldl (++) [] bs
-
-mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree f = foldTree f'
-    where f' x bs = T (f x) bs
-
-trimTree :: Int -> Tree a -> Tree a
-trimTree 1 (T x _) = (T x [])
-trimTree n (T x ts) = T x (map (trimTree (n-1)) ts)
-
-path :: [Int] -> Tree a -> a
-path [] (T y _) = y
-path (x:xs) (T _ ts) = path xs (findXth x ts)
-    where findXth x _ | x < 0 = error "Negative child in path!"
-          findXth _ xs | null xs = error "Empty children list!"
-          findXth 0 (x:_) = x
-          findXth x (_:xs) = findXth (x-1) xs
-
--- Exercise 3
 
 instance Arbitrary a => Arbitrary (Tree a) where
     arbitrary = sized tree
@@ -103,8 +40,8 @@ prop_mapTree f t = sizeTree t == sizeTree t' &&
                    heightTree t == heightTree t'
     where t' = mapTree f t
 
-prop_mapTree2 :: Int -> (Int -> Int) -> Tree Int -> Property
-prop_mapTree2 n f t = inTree n t ==> inTree n' t'
+prop_mapTree2 :: (Int -> Int) -> Int -> Tree Int -> Property
+prop_mapTree2 f n t = inTree n t ==> inTree n' t'
     where n' = f n
           t' = mapTree f t
 
@@ -119,11 +56,13 @@ prop_nodes_leaves :: (Int -> Int) -> Tree Int -> Bool
 prop_nodes_leaves f t = (map f . nodes) t == (nodes . mapTree f) t &&
                         (map f . leaves) t == (leaves . mapTree f) t
 
-
+-- A fucntion that create lazily the Bird Tree
+-- The Bird Tree: (http://www.cs.ox.ac.uk/ralf.hinze/publications/Bird.pdf)
 bird :: Tree Rational
 bird = T 1 [mapTree (\x -> 1 / x) (mapTree (+1) bird),
             mapTree (+1) (mapTree (\x -> 1 / x) bird)]
 
+-- A generator for random paths in a binary tree
 genPaths :: Gen [Int]
 genPaths = sized $ \x -> if x > 0
                             then vectorOf x $ choose (0,1)
@@ -134,6 +73,7 @@ prop_bird_path t = forAll genPaths $ bird_path
                     where bird_path xs = path xs t == path xs trimmed_t
                             where trimmed_t = trimTree (length xs + 1) t
 
+-- A generator for a ZigZag path in a binary tree
 genZigZagPaths :: Gen [Int]
 genZigZagPaths = sized $ \x -> if x > 0
                                 then return $ concat (replicate (x*x*x) [1,0])
@@ -150,6 +90,7 @@ prop_all_naturals t = forAll genZigZagPaths $
                   findXth 0 (x:_) = x
                   findXth x (_:xs) = findXth (x-1) xs
 
+-- A simple (and infinite) implementation of the Fibonacci sequence
 fibs :: [Integer]
 fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
 
@@ -161,6 +102,9 @@ prop_fib_in_bird t = forAll (sized $
           bird_fibs n (T y ts) acc =
             bird_fibs (n-1) (head ts) ((denominator y):acc)
 
+-- Returns True if the Rational number given is found in the bird tree
+-- Otherwise, it doesn't terminate
+-- (All Rationals are in the bird tree so it should at some point...)
 findBird :: Rational -> Tree Rational -> Bool
 findBird q t = findBird' q t 1
     where findBird' q' t' n =
@@ -168,6 +112,7 @@ findBird q t = findBird' q t 1
                 then True
                 else findBird' q' t' (n+1)
 
+-- A simple generator for small Rational numbers with an upper bound given in seed
 genRationals :: Integer -> Gen Rational
 genRationals seed | seed > 0 = liftM2 (%) (choose (1, seed)) (choose (1, seed))
                   | otherwise = liftM2 (%) (choose (1, 23)) (choose (1, 23))
@@ -175,5 +120,42 @@ genRationals seed | seed > 0 = liftM2 (%) (choose (1, seed)) (choose (1, seed))
 prop_all_rationals :: Integer -> Tree Rational -> Property
 prop_all_rationals seed t = forAll (genRationals seed) $ \q -> findBird q t
 
-main = do t <- verboseCheck (prop_all_rationals 13 bird)
-          print t
+main = do
+        putStrLn "Check that the height of a tree is a positive integer and is less than the size of the Tree (prop_heightTree):"
+        t <- quickCheck prop_heightTree
+        print t
+        putStrLn "\nCheck that the maximum value of the tree is actually in the tree (prop_maxTree):"
+        t <- quickCheck prop_maxTree
+        print t
+        putStrLn "\nCheck that all tree nodes are actually in the tree (prop_nodesInTree):"
+        t <- quickCheck prop_nodesInTree
+        print t
+        putStrLn "\nCheck that the number of all nodes that satisfy a predicate f (e.g. x > 4) is an integer that is less than the size of the tree (prop_countTree):"
+        t <- quickCheck $ prop_countTree (>4)
+        print t
+        putStrLn "\nCheck that the number of the tree nodes is equal to its size"
+        putStrLn "Also check that the number of its leaves is less than the size of the tree, unless both numbers are 1 (prop_sizeTree):"
+        t <- quickCheck prop_sizeTree
+        print t
+        putStrLn "\nCheck that the function mapTree keeps the size and the height of the tree intact (prop_mapTree):"
+        t <- quickCheck $ prop_mapTree (+42)
+        print t
+        putStrLn "\nCheck that, if n is in the original tree, then the value f n (e.g. f x = x + 42) is in the mapped tree (prop_mapTree2):"
+        t <- quickCheck $ prop_mapTree2 (+42)
+        print t
+        putStrLn "\nCheck for functions g ∈ {\"nodes\", \"leaves\"} that \"map f . g == g. mapTree f\" (prop_nodes_leaves):"
+        t <- quickCheck $ prop_nodes_leaves (+42)
+        print t
+        putStrLn "\n\nGiven the Bird Tree that we implemented we have the following tests (bird):"
+        putStrLn "\nCheck if given a path of length n, when we follow it in \"bird\" or in \"trimTree n bird\" we get the same node (prop_bird_path):"
+        t <- quickCheck $ prop_bird_path bird
+        print t
+        putStrLn "\nCheck that following a ZigZag path in the Bird tree gives us all the natural numbers (prop_all_naturals):"
+        t <- quickCheck $ prop_all_naturals bird
+        print t
+        putStrLn "\nCheck that following the leftmost path in the Bird tree gives us the Fibonacci sequence (prop_fib_in_bird):"
+        t <- quickCheck $ prop_fib_in_bird bird
+        print t
+        putStrLn "\nCheck that every Rational number appears in the Bird tree (prop_all_rationals):"
+        t <- quickCheck $ prop_all_rationals 13 bird
+        print t
