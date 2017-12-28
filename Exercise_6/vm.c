96e2271ceef4f70c62abf6be04538b7baa17a8e9
gsakkas_git <george.p.sakkas@gmail.com>
2017-12-28 14:39:53 +0200

    Implemented all op-codes, Added Makefile

--- Exercise_6/vm.c
+++ Exercise_6/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <time.h>
 
 // op-codes
 #define HALT 0x00
@@ -56,6 +57,7 @@ int main(int argc, char const *argv[]) {
 	uint8_t *pc = &program[0];
 	uint8_t opcode;
 	bool loop = true;
+	clock_t start_time = clock();
 	while (loop) {
 		opcode = pc[0];
 		switch (opcode) {
@@ -110,39 +112,146 @@ int main(int argc, char const *argv[]) {
 				break;
 			}
 			case ADD:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				push(stack, top, a + b);
+				pc += 1;
 				break;
+			}
 			case SUB:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				push(stack, top, a - b);
+				pc += 1;
 				break;
+			}
 			case MUL:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				push(stack, top, a * b);
+				pc += 1;
 				break;
+			}
 			case DIV:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				push(stack, top, a / b);
+				pc += 1;
 				break;
+			}
 			case MOD:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				push(stack, top, a % b);
+				pc += 1;
 				break;
+			}
 			case EQ:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				int eq = (a == b) ? 1 : 0;
+				push(stack, top, eq);
+				pc += 1;
 				break;
+			}
 			case NE:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				int eq = (a != b) ? 1 : 0;
+				push(stack, top, eq);
+				pc += 1;
 				break;
+			}
 			case LT:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				int eq = (a < b) ? 1 : 0;
+				push(stack, top, eq);
+				pc += 1;
 				break;
+			}
 			case GT:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				int eq = (a > b) ? 1 : 0;
+				push(stack, top, eq);
+				pc += 1;
 				break;
+			}
 			case LE:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				int eq = (a <= b) ? 1 : 0;
+				push(stack, top, eq);
+				pc += 1;
 				break;
+			}
 			case GE:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				int eq = (a >= b) ? 1 : 0;
+				push(stack, top, eq);
+				pc += 1;
 				break;
+			}
 			case NOT:
+			{
+				int stack_top = pop(stack, top);
+				int not = (stack_top == 0) ? 1 : 0;
+				push(stack, top, not);
+				pc += 1;
 				break;
+			}
 			case AND:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				int and = (a && b) ? 1 : 0;
+				push(stack, top, and);
+				pc += 1;
 				break;
+			}
 			case OR:
+			{
+				int a = pop(stack, top);
+				int b = pop(stack, top);
+				int or = (a || b) ? 1 : 0;
+				push(stack, top, or);
+				pc += 1;
 				break;
+			}
 			case INPUT:
+			{
+				char ch;
+				if (scanf("%c", &ch) == 1);
+				push(stack, top, (int32_t)ch);
+				pc += 1;
 				break;
+			}
 			case OUTPUT:
+			{
+				int32_t ch = pop(stack, top);
+				pc += 1;
 				break;
+			}
 			case CLOCK:
+			{	
+				double time_spent = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+				printf("%0.6lf\n", time_spent);
+				pc += 1;
 				break;
+			}
 		}
 	}
 	return 0;
