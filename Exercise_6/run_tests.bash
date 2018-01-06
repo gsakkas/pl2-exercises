674bc5d9bf2968640211ed8062e52a7bcc526166
gsakkas_git <george.p.sakkas@gmail.com>
2018-01-06 20:25:01 +0200

    Added other versions to Makefile and fixed tests

--- Exercise_6/run_tests.bash
+++ Exercise_6/run_tests.bash
@@ -1,5 +1,11 @@
-for n in {1..5}
-do
-	/usr/bin/time --format="%E" ./vm big_loop.b > test; 
-done
-
+if [ $# -eq 0 ]; then
+	for n in {1..5}
+	do
+		/usr/bin/time --format="%E" ./vm_switch big_loop.b > test;
+	done
+else
+	for n in {1..5}
+	do
+		/usr/bin/time --format="%E" ./$1 big_loop.b > test;
+	done
+fi
