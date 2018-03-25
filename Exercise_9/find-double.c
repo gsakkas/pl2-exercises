134696d0352adc3d5274440bd0090cc71034cef8
gsakkas_git <george.p.sakkas@gmail.com>
2018-03-25 23:00:20 +0300

    Finished Exercise 9 with Frama-C

--- Exercise_9/find-double.c
+++ Exercise_9/find-double.c
@@ -8,36 +8,36 @@
 
 #define MAXV 1000000
 
-/*@ predicate foundDoubles{L}(integer N, int* a) =
-  @		\exists integer i; \exists integer j; 0 <= i < N && 0 <= j < N && i != j ==> a[i] == a[j];
+/*@ predicate doublesInside(integer N, int* a, integer res) =
+  @		\exists integer ii; \exists integer jj;
+  @			0 <= ii < N && 0 <= jj < N && ii != jj ==> a[ii] == a[jj] == res;
   @*/
 
-/*@ logic integer check_if_exists{L}(integer N, int* a, int d) =
-  @		(N == 0) ? 0 :
-  @			(a[0] == d) ? 1 : check_if_exists(N-1, a+1, d);
+/*@ predicate noDoublesInside(integer N, int* a) =
+  @		\forall integer ii; \forall integer jj;
+  @			0 <= ii < N && 0 <= jj < N && ii != jj ==> a[ii] != a[jj];
   @*/
 
-/*@ logic integer doubles{L}(integer N, int* a) =
-  @		(N <= 1) ? 0 :
-  @  		(check_if_exists(N-1, a+1, a[0]) == 1) ?
-  @				a[0] : doubles(N-1, a+1);
+/*@ predicate doublesFound(integer N, int* a, integer res) =
+  @		(res > 0) ? doublesInside(N, a, res) : noDoublesInside(N, a);
   @*/
 
 /*@ requires 1 <= N <= MAXV;
   @	requires \valid(a + (0..N-1));
   @ requires \forall integer p; 0 <= p < N ==> 1 <= a[p] <= MAXV;
-  @	ensures \result == doubles(N, a);
+  @	ensures  doublesFound(N, a, \result);
   @*/
 int findDouble(int N, int a[]) {
 	bool f[MAXV];
 	/*@ loop invariant	1 <= i <= MAXV+1;
+	  @ loop invariant	\forall integer p; 1 <= p < i ==> f[p-1] == false;
 	  @ loop assigns	i, f[0..(MAXV-1)];
 	  @ loop variant	MAXV - i;
 	  @*/
 	for (int i = 1; i <= MAXV; ++i) f[i-1] = false;
 	/*@ loop invariant	0 <= i <= N;
 	  @ loop invariant	\forall integer p; 0 <= p < i ==> f[a[p]-1] != false;
-	  @ loop invariant	!foundDoubles(i, a);
+	  @ loop invariant	noDoublesInside(i, a);
 	  @ loop assigns	i, f[0..(MAXV-1)];
 	  @ loop variant	N - i;
 	  @*/
@@ -45,15 +45,3 @@ int findDouble(int N, int a[]) {
 		if (f[a[i]-1]) return a[i]; else f[a[i]-1] = true;
 	return 0;
 }
-
-/*@ lemma doubs_in_empty:
-  @		\forall int* p; \valid(p) ==> !foundDoubles(0, p);
-  @*/
-
-/*@ lemma doubles_in_empty:
-  @		\forall int* p; \valid(p) ==> doubles(0, p) == 0;
-  @*/
-
-/*@ lemma doubles_in_one_elem:
-  @		\forall int* p; \valid(p) ==> doubles(1, p) == 0;
-  @*/
